3fe7db13edab5ceea2b4c1b2e8a25265c5953c45
Tsukasa Arakawa <s23c1004ac@s.chibakoudai.jp>
2024-10-31 13:15:47 +0900

    Change cord(1)

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2024 Tsukasa Arakawa <s23c1004ac@s.chibakoudai.jp>　　　
 # SPDX-License-Identifier: GPL-3.0-only
 
@@ -12,5 +12,14 @@ res=0
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
 
+
+out=$(echo あ| ./plus)
+[ "$?" = 1 ]	  || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
 [ "${res}" = 0 ] && echo OK
-exit $res
+exit $res 
